d5528b94d8ac88b6a457a50b31fa30e64ab3ca53
= <nathaniel.risum2@gmail.com>
2024-09-09 09:35:30 +0200

    Finalize test

--- ClassRoomNet60Tests/StudentTests.cs
+++ ClassRoomNet60Tests/StudentTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace ClassRoomNet60.Tests
 {
@@ -20,6 +21,21 @@ namespace ClassRoomNet60.Tests
             Student s = new Student("Test", month, 15);
             string output = s.Season;
             Assert.AreEqual("Winter", output); 
+        }
+
+        [TestMethod()]
+        [DataRow(0)]
+        [DataRow(13)]
+        [DataRow(-1)]
+        public void StudentSeasonInvalidTest(int badmonth)
+        {
+            ; 
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+            {
+                Student s = new Student("Test", badmonth, 15);
+                string output = s.Season; 
+            });
+
         }
     }
 }
